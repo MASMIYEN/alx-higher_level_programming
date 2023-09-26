191ba5bb5c53f9d91c1bb82361d6d80eaba47308
masmiyen <masmiyen@gmail.com>
2023-09-26 20:43:30 +0100

    Coordinates of a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,43 +1,32 @@
 #!/usr/bin/python3
-"""square"""
+"""Square"""
 
 
 class Square:
-    """define square"""
+    """defining square"""
 
     def __init__(self, size=0):
-        """construct
-        Args:
-            size: length of square
-        """
-        self.size = size
+        """initialization function"""
+        self.__size = size
 
     @property
     def size(self):
-        """ length of a side of the square
-        Raises:
-            TypeError: is size is not an integer
-            ValueError: if size is less than zero
-        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError('size must be an integer')
+        if type(value) != int:
+            raise TypeError("size must be an integer")
         if value < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = value
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     def area(self):
-        """Area of the square
-        Returns:
-            The size squared
-        """
+        """returns area of square"""
         return self.__size ** 2
 
     def my_print(self):
-        """print the square"""
         if self.__size == 0:
             print()
         else:
--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,57 +1,62 @@
 #!/usr/bin/python3
-"""Square"""
+"""square"""
 
 
 class Square:
     """define square"""
 
     def __init__(self, size=0, position=(0, 0)):
-        """initialization function"""
-        self.__size = size
-        self.__position = position
+        """construct
+        Args:
+            size: length of square
+        """
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
+        """ length of a side of the square
+        Raises:
+            TypeError: is size is not an integer
+            ValueError: if size is less than zero
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        if type(value) != int:
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
-
-    def area(self):
-        """returns area of square"""
-        return self.__size ** 2
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
 
     @property
     def position(self):
+        """get the current position"""
         return self.__position
 
     @position.setter
     def position(self, value):
-        if (
-                not isinstance(value, tuple)
-                or len(value) != 2
-                or not all(isinstance(x, int) and x >= 0 for x in value)
-        ):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
+    def area(self):
+        """Area of the square
+        Returns:
+            The size squared
+        """
+        return self.__size ** 2
+
     def my_print(self):
-        space = ""
+        """print the square"""
         if self.__size == 0:
             print()
         else:
-            for i in range(self.__position[1]):
-                space += "\n"
             for i in range(self.__size):
-                for j in range(self.__position[0]):
-                    space += " "
                 for j in range(self.__size):
-                    space += "#"
-                space += "\n"
-            print("{}".format(space), end="")
+                    print("#", end="")
+                print()
