71153a6f47bf168d4b19e888ab7122822e8194d0
masmiyen <masmiyen@gmail.com>
2023-10-09 16:10:53 +0100

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """class with 1 attr"""
+
+    def area(self):
+        """raises exception with message"""
+        raise Exception("area() is noy implemented")
