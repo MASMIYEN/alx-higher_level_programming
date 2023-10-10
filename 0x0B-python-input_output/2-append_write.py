db0843035792f09a7ab01e62929be6b24c8fd1ba
masmiyen <masmiyen@gmail.com>
2023-10-10 13:54:59 +0100

    Append to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""append file with 2 args definition"""
+
+
+def append_write(filename="", text=""):
+    """append filename with utf-8 encoding"""
+    with open(filename, "a", encoding='utf-8') as file:
+        return file.write(text)
