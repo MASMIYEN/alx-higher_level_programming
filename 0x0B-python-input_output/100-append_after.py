a7b163cb2bc3066caf6276e86af9834d3bb2f2e7
masmiyen <masmiyen@gmail.com>
2023-10-10 22:03:23 +0100

    fixed newline error for pycodestyle

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""Module with append_after function"""
+"""Defines a text file insertion function."""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """inserts line of text to a file after specific string"""
-    with open(filename, "r") as myFile:
-        reader = myFile.readlines()
-    with open(filename, "w") as modFile:
-        words = ""
-        for l in reader:
-            words += l
-            if search_string in l:
-                words += new_string
-        modFile.write(words)
+    """Insert text after each line containing a given string in a file.
+
+    Args:
+        filename (str): The name of the file.
+        search_string (str): The string to search for within the file.
+        new_string (str): The string to insert.
+    """
+    text = ""
+    with open(filename) as r:
+        for line in r:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as w:
+        w.write(text)
