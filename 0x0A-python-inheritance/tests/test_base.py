3cbda8d348d51579454b4a18d56ef3b40366183b
masmiyen <masmiyen@gmail.com>
2023-10-16 08:13:59 +0100

    Let's draw it

--- 0x0A-python-inheritance/tests/test_base.py
+++
@@ -1,6 +0,0 @@
-from unittest import TestCase
-
-
-class TestBase(TestCase):
-    def test_to_json_string(self):
-        self.fail()
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""""" Module for Base tests ""
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
