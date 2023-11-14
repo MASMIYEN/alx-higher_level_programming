7bd0f34d450edebb8f1e1666a80ed51a77436fa2
masmiyen <masmiyen@gmail.com>
2023-11-14 19:54:41 +0100

    Temperatures #0

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,2 @@
+-- Displays the average temperature by city ordered by descending temperature
+SELECT `city`, AVG(`value`) AS `avg_temp` FROM `temperatures` GROUP BY `city` ORDER BY `avg_temp` DESC;
