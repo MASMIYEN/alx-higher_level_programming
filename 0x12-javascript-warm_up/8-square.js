8108ada10abe21167fb071942a516525644934bb
MASMIYEN <masmiyen@gmail.com>
2023-12-11 17:18:42 +0100

    2 tabs

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const { argv } = require('process');
-const str = 'X';
+const Str = 'X';
 const x = parseInt(argv[2], 10);
 if (!isNaN(x)) {
 	for (let i = 0; i < x; i++) {
-		console.log(str.repeat(X));
+		console.log(str.repeat(x));
 	}
 } else console.log('Missing size');
