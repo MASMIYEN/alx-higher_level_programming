755ca1c0d17931fa292dcba51c0c9157a9015eb6
MASMIYEN <masmiyen@gmail.com>
2023-12-11 07:29:50 +0100

    more tasks done

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const { argv } = require('process');
+const i = parseInt(argv[2], 10);
+function factorial (i) {
+    if (i <= 1) return 1;
+    return i * factorial(i - 1);
+}
+if (isNaN(i)) console.log(1);
+else console.log(factorial(i));
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+const { argv } = require('process');
+const i = parseInt(argv[2], 10);
+const j = parseInt(argv[3], 10);
+function add (i, j) {
+    return i + j;
+}
+console.log(add(i, j));
