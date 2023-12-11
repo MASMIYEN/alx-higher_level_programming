7bf64980093f233c2adca738be2481c4d4711bee
MASMIYEN <masmiyen@gmail.com>
2023-12-11 06:50:09 +0100

    more tasks done

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const { argv } = require('process');
+const str = 'C is fun';
+let x = parseInt(argv[2], 10);
+if (!isNaN(x)) {
+    while (x > 0) {
+        console.log(str);
+        x--;
+    }
+} else console.log('Misssing number of occurences');
