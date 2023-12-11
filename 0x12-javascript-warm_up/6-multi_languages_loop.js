7e9988d494eff20c0e9442072147187d9e17931f
MASMIYEN <masmiyen@gmail.com>
2023-12-11 17:13:07 +0100

    2 tabs

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const strArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < strArray.length; i++) {
-    console.log(strArray[i]);
+  console.log(strArray[i]);
 }
