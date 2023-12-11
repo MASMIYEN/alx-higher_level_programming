2481058507f91ac010e4b440ab0a6f869c804685
MASMIYEN <masmiyen@gmail.com>
2023-12-11 16:53:03 +0100

    arg

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const { arg } = require ('process');
+const { argv } = require ('process');
 const firstArg = argv[2];
-const secondArg = arg[3];
+const secondArg = argv[3];
 const midArg = ' is ';
 console.log(firstArg + midArg + secondArg);
