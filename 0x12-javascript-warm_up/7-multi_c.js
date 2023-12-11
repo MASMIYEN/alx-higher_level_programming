51686b1bed6eafefb201e0113d58585911e9d689
MASMIYEN <masmiyen@gmail.com>
2023-12-11 17:15:13 +0100

    2 tabs

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const str = 'C is fun';
 let x = parseInt(argv[2], 10);
 if (!isNaN(x)) {
   while (x > 0) {
-      console.log(str);
-      x--;
+    console.log(str);
+    x--;
   }
 } else console.log('Misssing number of occurences');
