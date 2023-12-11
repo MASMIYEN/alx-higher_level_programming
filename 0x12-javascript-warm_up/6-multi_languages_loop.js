2d3024b050a19eeefb77d2f52ebb6616aa7e0e1c
MASMIYEN <masmiyen@gmail.com>
2023-12-11 16:58:38 +0100

    pithon

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const strArray = ['C is fun', 'Pyhton is cool', 'JavaScript is amazing'];
+const strArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < strArray.length; i++) {
     console.log(strArray[i]);
 }
