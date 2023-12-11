068adcb946e2b13fa3762434436792b00f691999
MASMIYEN <masmiyen@gmail.com>
2023-12-11 17:04:52 +0100

    typo

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const myVar1 = 'C is fun';
-const myVar2 = 'Python is coool';
+const myVar2 = 'Python is cool';
 const myVar3 = 'JavaScript is Amazing';
 console.log(myVar1);
 console.log(myVar2);
