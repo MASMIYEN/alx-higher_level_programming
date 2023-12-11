3dc6ca2f417de15f025ab4125d02b431890c8e06
MASMIYEN <masmiyen@gmail.com>
2023-12-11 17:20:28 +0100

    tabs and eof

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,6 +3,6 @@ const { argv } = require('process');
 const i = parseInt(argv[2], 10);
 const j = parseInt(argv[3], 10);
 function add (i, j) {
-    return i + j;
+  return i + j;
 }
 console.log(add(i, j));
