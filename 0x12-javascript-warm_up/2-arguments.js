13fdc1e241ef00e34a3926ba9056ffde2bbc2126
MASMIYEN <masmiyen@gmail.com>
2023-12-11 17:06:59 +0100

    typo

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const { argv } = require('process');
 if (argv.length <= 2) console.log('No argument');
-if (argv.length === 3) console.log('Argument Found');
+if (argv.length === 3) console.log('Argument found');
 if (argv.length > 3) console.log('Arguments found');
