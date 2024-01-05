b824a702956af05fd303494f6545da48b64be28a
MASMIYEN <masmiyen@gmail.com>
2024-01-06 00:28:27 +0100

    py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,17 +1,16 @@
-#!/bin/bash/python3
+#!/usr/bin/python3
 """ module doc """
 
 
 def find_peak(list_of_integers):
-    """ doc def"""
+    """def doc"""
     if list_of_integers:
         lf = 0
         r = len(list_of_integers) - 1
         while lf < r:
             m = (lf + r) // 2
-            if list_of_integers[m] < list_of_integers[m + 1]:
-                lf = m + 1
-            else:
+            if list_of_integers[m] > list_of_integers[m + 1]:
                 r = m
-
+            else:
+                lf = m + 1
         return list_of_integers[lf]
