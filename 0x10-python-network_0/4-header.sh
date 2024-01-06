2c5320ce395931cf81df82d41aff61e0bbcc94be
MAsmiyen <123839250+MASMIYEN@users.noreply.github.com>
2024-01-06 06:06:19 +0100

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Send a GET request to a given URL with a header variable.
 #curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
-curl -sLX PUT -H "Origin: HolbertonSchool" -d "User_Id=98" 0.0.0.0:5000/catch_me```
+curl -sLX PUT -H "Origin: HolbertonSchool" -d "User_Id=98" 0.0.0.0:5000/catch_me
