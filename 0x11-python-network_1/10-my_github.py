e4e817129d65a44028a00bcddd099065be85329e
MASMIYEN <masmiyen@gmail.com>
2024-01-08 00:43:40 +0100

    another one

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,21 +6,6 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
-    # Check if the user provided enough command line arguments
-    if len(sys.argv) < 3:
-        print("Usage: ./10-my_github.py <GitHub username> <GitHub password>")
-        sys.exit(1)
-
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-
-    # Send the GET request
     response = requests.get("https://api.github.com/user", auth=auth)
-
-    try:
-        # Raise an exception for HTTP errors
-        response.raise_for_status()
-
-        # Print the user's GitHub ID
-        print(response.json().get("id"))
-    except requests.exceptions.HTTPError as err:
-        print(f"HTTP error occurred: {err}")
+    print(response.json().get("id"))
