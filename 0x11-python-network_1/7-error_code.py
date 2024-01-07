a4b51ba181b1cc32f52f585403109b83fc1618a2
MASMIYEN <masmiyen@gmail.com>
2024-01-07 23:14:56 +0100

    8 th task

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Sends a request to a given URL and displays the response body.
+
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    r = requests.get(url, timeout=5)  # Added timeout argument with a value of 5 seconds
+    if r.status_code >= 400:
+        print(f"Error code: {r.status_code}")
+    else:
+        print(r.text)
