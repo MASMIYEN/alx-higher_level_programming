6db75f3514179c4df01cc1796f0de41af0dca4ae
MASMIYEN <masmiyen@gmail.com>
2024-01-08 00:51:35 +0100

    fix from ouss

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,11 +8,13 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    user = sys.argv[1]
+    username = sys.argv[1]
     password = sys.argv[2]
-    headers = {'Authorization': f'Bearer {password}'}
-    res = requests.get("https://api.github.com/user", headers=headers)
-    if (res.status_code >= 400):
-        print("None")
-        exit()
-    print(res.json()["id"])
+    url = f"https://api.github.com/users/{username}"
+    headers = {"Authorization": f"Bearer {password}"}
+
+    r = requests.get(
+        url,
+        headers=headers,
+    )
+    print(r.json().get("id"))
