c3f9a54c21be42f4846d5a210c9ce8101a91b9d2
MASMIYEN <masmiyen@gmail.com>
2024-01-08 00:37:40 +0100

    fix

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,29 +1,14 @@
 #!/usr/bin/python3
-"""Lists the 10 most recent commits on a given GitHub repository.
-"""
 import sys
 import requests
 
 if __name__ == "__main__":
-    # Check if the user provided enough command line arguments
-    if len(sys.argv) < 3:
-        print("Usage: ./100-github_commits.py <repository name> <repository owner>")
-        sys.exit(1)
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
 
-    url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits?per_page=10"
-
-    # Send the GET request
+    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits?per_page=10"
     response = requests.get(url)
 
-    try:
-        # Raise an exception for HTTP errors
-        response.raise_for_status()
-
-        # Get the list of commits
-        commits = response.json()
-
-        # Print the SHA and author name for each commit
-        for commit in commits:
-            print(f"{commit.get('sha')}: {commit.get('commit').get('author').get('name')}")
-    except requests.exceptions.HTTPError as err:
-        print(f"HTTP error occurred: {err}")
+    commits = response.json()
+    for commit in commits:
+        print(f"{commit.get('sha')}: {commit.get('commit').get('author').get('name')}")
